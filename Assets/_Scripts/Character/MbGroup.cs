bf8e81fef4a786e3aad9bc2c54cc935cec63dc23
bacdepzai <ngobac20031016@gmail.com>
2025-02-09 18:15:49 +0700

    fix

--- Assets/_Scripts/Character/MbGroup.cs
+++ Assets/_Scripts/Character/MbGroup.cs
@@ -6,6 +6,7 @@ using Cysharp.Threading.Tasks;
 using System;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Threading;
 
 namespace HitAndRun.Character
 {
@@ -14,15 +15,15 @@ namespace HitAndRun.Character
     {
         public override void OnInspectorGUI()
         {
-            DrawDefaultInspector();
             var group = (MbGroup)target;
-            EditorGUILayout.Space();
             GUI.enabled = Application.isPlaying;
             if (GUILayout.Button("Add Character"))
             {
                 group.AddCharacter();
             }
             GUI.enabled = true;
+            EditorGUILayout.Space();
+            DrawDefaultInspector();
         }
     }
 
@@ -33,14 +34,21 @@ namespace HitAndRun.Character
         [SerializeField] private GameObject _prefab;
         [SerializeField] private List<MbCharacter> _row = new();
         private Queue<MbCharacter> _pool = new();
+        private Queue<(MbCharacter, int)> _insertQueue = new();
+        [SerializeField] private Transform _follow;
+        private CancellationToken _ctk;
         private void Reset()
         {
             _prefab = Resources.Load<GameObject>("Prefabs/Character");
+            _follow = transform.Find("Follow");
         }
         private void Start()
         {
             _row.Add(SpawnCharacter(transform.position));
+            _ctk = this.GetCancellationTokenOnDestroy();
+            ProcessInsertCharactes();
         }
+
 
         private MbCharacter SpawnCharacter(Vector3 position)
         {
@@ -64,9 +72,22 @@ namespace HitAndRun.Character
             _pool.Enqueue(character);
         }
 
+
+        private async void ProcessInsertCharactes()
+        {
+            while (!_ctk.IsCancellationRequested)
+            {
+                await UniTask.WaitUntil(() => _insertQueue.Count != 0);
+                var (character, index) = _insertQueue.Dequeue();
+                await ProcessInsertCharacterAt(character, index);
+            }
+        }
+
         private async Task MergeCharacter(int one, int two)
         {
-            if (Mathf.Abs(_row[one].transform.position.x) < Mathf.Abs(_row[two].transform.position.x))
+            var center = _follow.localPosition.x;
+            if (Mathf.Abs(_row[one].transform.localPosition.x - center)
+            < Mathf.Abs(_row[two].transform.localPosition.x - center))
                 (two, one) = (one, two);
 
             _row[two].Body.Level *= 2;
@@ -74,7 +95,8 @@ namespace HitAndRun.Character
             var target = _row[two].transform.localPosition;
             await _row[one].transform
                 .DOLocalMove(target, 0.3f)
-                .SetEase(Ease.OutQuad).ToUniTask();
+                .SetEase(Ease.OutQuad)
+                .WithCancellation(_ctk);
 
             DespawnCharacter(_row[one]);
         }
@@ -83,31 +105,76 @@ namespace HitAndRun.Character
         {
             if (_row.Count == 0) return;
 
-            var start = -0.5f * (_row.Sum(c => c.Body.Width) + (_row.Count - 1) * _offset);
+            var center = 0;
+            var min = Mathf.Abs(_row[0].transform.localPosition.x - _follow.localPosition.x);
+            for (int i = 1; i < _row.Count; i++)
+            {
+                var distance = Mathf.Abs(_row[i].transform.localPosition.x - _follow.localPosition.x);
+                if (distance < min)
+                {
+                    min = distance;
+                    center = i;
+                }
+            }
+
+            var positions = new float[_row.Count];
+            positions[center] = _row[center].transform.localPosition.x;
+
+            for (int i = center + 1; i < _row.Count; i++)
+                positions[i] = positions[i - 1] + _row[i - 1].Body.Width + _offset;
+
+            for (int i = center - 1; i >= 0; i--)
+                positions[i] = positions[i + 1] - _row[i].Body.Width - _offset;
 
             var tasks = _row.Select((character, i) =>
-            {
-                var target = new Vector3(start + character.Body.Width * 0.5f, 0, 0);
-
-                start += character.Body.Width + _offset;
-
-                return character.transform
-                    .DOLocalMove(target, 0.3f)
+                character.transform
+                    .DOLocalMove(positions[i] * Vector3.right, 0.3f)
                     .SetEase(Ease.OutQuad)
-                    .ToUniTask();
-            });
+                    .WithCancellation(_ctk)
+            );
 
             await UniTask.WhenAll(tasks);
         }
 
-        public async void AddCharacterAt(MbCharacter character, int index)
+        public void InsertCharacterAt(MbCharacter character, int index)
         {
-            if (index < 0 || index > _row.Count) return;
-            character.transform.SetParent(transform);
-            _row.Insert(index, character);
-            await RearrangeCharacters();
+            _insertQueue.Enqueue((character, index));
+        }
 
-            bool merged;
+        private async UniTask ProcessInsertCharacterAt(MbCharacter character, int index)
+        {
+            if (index < -1 || index > _row.Count) return;
+            index = (index == -1) ? _row.Count : index;
+
+            character.transform.SetParent(transform);
+            var tasks = new List<UniTask>();
+            var insert = (index == _row.Count ? _row[^1] : _row[index]).transform.localPosition.x;
+            var gap = _offset + character.Body.Width * 0.5f;
+
+            if (index == 0) insert -= gap + _row[index].Body.Width * 0.5f;
+            else if (index == _row.Count) insert += gap + _row[index - 1].Body.Width * 0.5f;
+            else
+            {
+                insert -= 0.5f * (gap + _row[index].Body.Width * 0.5f);
+                for (int i = 0; i < _row.Count; i++)
+                {
+                    var offset = Mathf.Sign(i - index) * 0.5f * (gap + _row[i].Body.Width * 0.5f);
+                    tasks.Add(_row[i].transform
+                        .DOLocalMove((_row[i].transform.localPosition.x + offset) * Vector3.right, 0.3f)
+                        .SetEase(Ease.OutQuad)
+                        .WithCancellation(_ctk));
+                }
+            }
+
+            tasks.Add(character.transform
+                .DOLocalMove(insert * Vector3.right, 0.3f)
+                .SetEase(Ease.OutQuad)
+                .WithCancellation(_ctk));
+
+            await UniTask.WhenAll(tasks);
+            _row.Insert(index, character);
+
+            bool merged, needArrange = false;
             do
             {
                 merged = false;
@@ -116,25 +183,26 @@ namespace HitAndRun.Character
                     await MergeCharacter(index, index - 1);
                     index--;
                     merged = true;
-
                 }
                 else if (index < _row.Count - 1 && _row[index + 1].Body.Level == _row[index].Body.Level)
                 {
                     await MergeCharacter(index + 1, index);
                     merged = true;
                 }
+                if (merged && !needArrange) needArrange = true;
 
             } while (merged);
+            if (needArrange) await RearrangeCharacters();
 
-            await RearrangeCharacters();
+            var center = _row.Average(c => c.transform.localPosition.x);
+            _follow.localPosition = new Vector3(center, _follow.localPosition.y, _follow.localPosition.z);
         }
+
 
         public void AddCharacter()
         {
-            var character = SpawnCharacter(transform.position + new Vector3(0, 0, 8f));
-            AddCharacterAt(character, UnityEngine.Random.Range(0, _row.Count + 1));
+            var c1 = SpawnCharacter(transform.position + new Vector3(0, 0, 8f));
+            InsertCharacterAt(c1, -1);
         }
-
-
     }
 }
